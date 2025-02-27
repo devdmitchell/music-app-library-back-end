6bbede25a6bd7d4db8cad71eaf8dd3a2f0cb5d36
devdmitchell <dmitchell.prsvr@gmail.com>
2025-02-27 10:23:40 -0600

    completed schema

---
+++ models/User.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+
+const UserSchema = new mongoose.Schema({
+    username: String,
+    password: String
+  })
+
+  const SongSchema = new mongoose.Schema(
+    {
+    title: String,
+    artist: String,
+    album: String,
+    cover: String,
+    userId: String,
+    favorite: Boolean
+  }
+)
+
+module.exports = mongoose.model('User', UserSchema)
+module.exports = mongoose.model('Song', SongSchema)
