09f0581df462f78c71b61e877e2b6c2716eafdb7
devdmitchell <dmitchell.prsvr@gmail.com>
2025-03-23 13:41:12 -0500

    upped limit

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router()
 
 const loginLimiter = rateLimit({
     windowMs: 24 * 60 * 60 * 1000, // 24 hours, resets after every 24 hours
-    max: 3, // Limits the user to 3 login attempts per IP address per day
+    max: 1000, // Limits the user to 100 login attempts per IP address per day
     message: { message: 'Too many login attempts. Please try again in 24 hours.' },
     standardHeaders: true,
     legacyHeaders: false,
